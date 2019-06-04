91a0cab6d36978ddc862945e7d5d1c9819f8c864
이예지 <mesvie50@gmail.com>
2019-06-04 19:06:30 +0900

    login Ok

--- src/main/java/google/config/SecurityConfig.java
+++ src/main/java/google/config/SecurityConfig.java
@@ -4,10 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Filter ssoFilter;
+
+    public SecurityConfig(Filter ssoFilter) {
+        this.ssoFilter = ssoFilter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,8 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().headers().frameOptions().sameOrigin()
-                .and().csrf().disable();
-//                .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class); // OAuthConfig에서 생성한 ssoFilter 추가
+                .and().csrf().disable()
+                .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class); // OAuthConfig에서 생성한 ssoFilter 추가
 
     }
 }
