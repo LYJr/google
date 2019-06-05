cdddc0aa035290a2cda9cc398ad423459526ea57
이예지 <mesvie50@gmail.com>
2019-06-05 15:56:33 +0900

    json ko

--- src/main/java/google/config/successHandler/GoogleAuthenticationSuccessHandler.java
+++ src/main/java/google/config/successHandler/GoogleAuthenticationSuccessHandler.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 public class GoogleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -27,14 +28,13 @@ public class GoogleAuthenticationSuccessHandler implements AuthenticationSuccess
     @Override //구글 계정 세션 저장
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         httpSession.setAttribute("user", getGoogleUser(authentication));
-        response.sendRedirect("/success");
+        response.sendRedirect("/ok");
     }
 
     // OAuth 인증정보를 통해 객체 생성
-    // Authentication로 구글 계정 정보 얻음 [단 map으로 되어있음]
+    // Authentication로 구글 계정 정보 얻음 [단 map으로 되어있음]ptj
     private GoogleUser getGoogleUser(Authentication authentication){
         OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) authentication;
-        return objectMapper.convertValue
-                (oAuth2Authentication.getUserAuthentication().getDetails(), GoogleUser.class);
+        return objectMapper.convertValue(oAuth2Authentication.getUserAuthentication().getDetails(), GoogleUser.class);
     }
 }
